84e16f1f0bc93a238dc02e830aa5a0262db3d81c
Tim <Tim@home-pc>
2015-04-13 22:21:50 +0100

    added foreign key

--- LocationTableGateway.php
+++ LocationTableGateway.php
@@ -9,42 +9,68 @@ class LocationTableGateway{
     }
     //s
     public function getLocations() {
-        // execute a query to get all events
-        $sqlQuery = "SELECT * FROM location";
-        
+        $sqlQuery =
+                "SELECT l.*, lm.lName AS manName
+                 FROM location l
+                 LEFT JOIN locationManager lm ON lm.venueID = l.locationID";
+
         $statement = $this->connection->prepare($sqlQuery);
         $status = $statement->execute();
-        
+
         if (!$status) {
-            die("Could not retrieve locations");
+            die("Could not retrieve programmers");
         }
-        //test 
+
         return $statement;
     }
     
-    public function getLocationById($id) {
-        
-        $sqlQuery = "SELECT * FROM location WHERE locationID = :id";
-        
+    public function getLocationManagerByLocationID($venueID) {
+        // execute a query to get all programmers
+        $sqlQuery =
+                "SELECT l.*, lm.lName AS manName
+                 FROM location l
+                 LEFT JOIN locationManager lm ON lm.venueID = l.locationID
+                 WHERE lm.venueID = :locationIDId";
+
+        $params = array(
+            'venueID' => $venueID
+        );
+        $statement = $this->connection->prepare($sqlQuery);
+        $status = $statement->execute($params);
+
+        if (!$status) {
+            die("Could not retrieve locations");
+        }
+
+        return $statement;
+    }
+
+    public function getLocationsByLocationID($locationID) {
+        $sqlQuery =
+                "SELECT l.*, lm.lNname AS manName
+                 FROM location l
+                 LEFT JOIN locationManager lm ON lm.venueID = l.venueID
+                 WHERE l.locationID = :venueID";
+
         $statement = $this->connection->prepare($sqlQuery);
         $params = array(
             "id" => $id
         );
-        
+
         $status = $statement->execute($params);
-        
+
         if (!$status) {
             die("Could not retrieve location");
         }
-        
+
         return $statement;
     }
-    
-    public function insertLocation($n, $a, $lma, $mn, $me, $mm){
+
+    public function insertLocation($n, $a, $lma, $mn, $me, $mm) {
         $sqlQuery = "INSERT INTO location " .
-                "(name, address, locMaxAttendees, manName, manEmail, manMobile)" .
+                "(name, address, locMaxAttendees, manName, manEmail, manMobile) " .
                 "VALUES (:name, :address, :locMaxAttendees, :manName, :manEmail, :manMobile)";
-        
+
         $statement = $this->connection->prepare($sqlQuery);
         $params = array(
             "name" => $n,
@@ -52,26 +78,26 @@ class LocationTableGateway{
             "locMaxAttendees" => $lma,
             "manName" => $mn,
             "manEmail" => $me,
-            "manMobile" => $mm            
+            "manMobile" => $mm,            
         );
 
         $status = $statement->execute($params);
 
-        if(!$status){
+        if (!$status) {
             die("Could not insert location");
         }
 
         $id = $this->connection->lastInsertId();
 
-        return $id;
+        return $locationID;
     }
-    
-    public function deleteLocation($id) {
-        $sqlQuery = "DELETE FROM location WHERE locationID = :id";
+
+    public function deleteLocation($locationID) {
+        $sqlQuery = "DELETE FROM location WHERE locationID = :locationID";
 
         $statement = $this->connection->prepare($sqlQuery);
         $params = array(
-            "id" => $id
+            "locationID" => $locationID
         );
 
         $status = $statement->execute($params);
@@ -82,31 +108,31 @@ class LocationTableGateway{
 
         return ($statement->rowCount() == 1);
     }
-    
-    public function updateLocation($id, $n, $a, $lma, $mn, $me, $mm){
-            $sqlQuery = 
+
+    public function updateLocation($lid, $n, $a, $lma, $mn, $me, $mm) {
+        $sqlQuery =
                 "UPDATE location SET " .
                 "name = :name, " .
                 "address = :address, " .
                 "locMaxAttendees = :locMaxAttendees, " .
-                "manName = :manName,  " .
+                "manName = :manName, " .
                 "manEmail = :manEmail, " .
-                "manMobile = :manMobile, " .             
+                "manMobile = :manMobile, " .                
                 "WHERE locationID = :locationID";
-            
-            $statement = $this->connection->prepare($sqlQuery);
-            $params = array(
-                "locationID" => $id,
-                "name" => $n,
-                "address" => $a,
-                "locMaxAttendees" => $lma,
-                "manName" => $mn,
-                "manEmail" => $me,
-                "manMobile" => $mm              
-            );
-            
-            $status = $statement->execute($params);
-            
-            return($statement->rowCount() == 1);
-    }      
+
+        $statement = $this->connection->prepare($sqlQuery);
+        $params = array(
+            "locationID" => $lid,
+            "name" => $n,
+            "address" => $a,
+            "locMaxAttendees" => $lma,
+            "manName" => $mn,
+            "manEmail" => $me,
+            "manMobile" => $mm,            
+        );
+
+        $status = $statement->execute($params);
+
+        return ($statement->rowCount() == 1);
+    }
 }
